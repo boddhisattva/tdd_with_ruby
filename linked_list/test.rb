f2a1a00e90416306ad139956e174ccde7ed07cb9
Mohnish G J <mail4mohnish@gmail.com>
2014-11-14 14:18:43 +0530

    checking indentation

--- linked_list/test.rb
+++ linked_list/test.rb
@@ -2,3 +2,15 @@ class Test1
   def testmeth
   end
 end
+
+class LinkList
+  attr_accessor :elements, :index
+  def initialize(val)
+    @elements = {}
+    @node = Node.new # each linked list has a node, 'has a' implying composition.
+    @node.data = val
+    @index = 0
+    @node.next_node = nil
+    @elements[@index] = @node
+  end
+end  
