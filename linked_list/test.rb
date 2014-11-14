c149e89992961db851b65ccb7f01a4c52466fea1
Mohnish G J <mail4mohnish@gmail.com>
2014-11-14 15:03:21 +0530

    the indented version of the singly linked list

--- linked_list/test.rb
+++ linked_list/test.rb
@@ -6,21 +6,23 @@ class LinkList
   attr_accessor :elements, :index
   def initialize(val)
     @elements = {}
-    @node = Node.new # each linked list has a node, 'has a' implying composition.
-    @node.data = val
+    get_node(val)
     @index = 0
-    @node.next_node = nil
     @elements[@index] = @node
   end
 
-  def add(val)
-    @node = Node.new #every new element that has to be added should point to a new node correponding to it.
+  def get_node(val)
+    @node = Node.new # each linked list has a node, 'has a' implying composition.
+    @node.data = val
+    @node.next_node = nil
+  end
+
+  def add(val)s
+    get_node(val) #every new element that has to be added should point to a new node correponding to it.
     list_size = @elements.size
     last_element_index = list_size - 1
     @elements[last_element_index].next_node = list_size
     new_element_index = list_size
-    @node.data = val
-    @node.next_node = nil
     @elements[new_element_index] =  @node
   end
 
@@ -30,7 +32,6 @@ class LinkList
 
   def list_elements
     puts "List elements - #{@elements}"
-    @elements
   end
 
   def list_count
@@ -38,11 +39,10 @@ class LinkList
   end
 
   a = LinkList.new(5)
-  puts a.add(3)
-  puts a.add(23)
+  a.add(3)
+  a.add(23)
   a.get_head
   a.list_elements
   a.list_count
   
 end
-end  
