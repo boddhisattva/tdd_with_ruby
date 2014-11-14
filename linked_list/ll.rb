5300a9fcf976f243eab671f34a540965c5cf23db
Mohnish G J <mail4mohnish@gmail.com>
2014-11-14 12:39:32 +0530

    making some changes to the way we are displaying the method results

--- linked_list/ll.rb
+++ linked_list/ll.rb
@@ -1,52 +1,47 @@
 class Node
-  attr_accessor :data, :next_node
-  # def initialize
-		# @data     
-  # 	@next_node = nil
-  # end
+	attr_accessor :data, :next_node
 end
 
 class LinkList
-  attr_accessor :elements, :index
+	attr_accessor :elements, :index
 	def initialize(val)
 		@elements = {}
-		@node = Node.new
+		@node = Node.new # each linked list has a node, 'has a' implying composition.
 		@node.data = val
 		@index = 0
 		@node.next_node = nil
 		@elements[@index] = @node
-		#node.next_node = (index += 1) 
-		
-  	#@elements[:@node.index] = data - this doesn't work.. 
 	end
 
-  def add(val)
-  	@node = Node.new #every new element that has to be added should point to a new node correponding to it.
-  	list_size = @elements.size
-  	last_element_index = list_size - 1
-  	@elements[last_element_index].next_node = list_size
-  	new_element_index = list_size
-  	@node.data = val
-  	@node.next_node = nil
+	def add(val)
+		@node = Node.new #every new element that has to be added should point to a new node correponding to it.
+		list_size = @elements.size
+		last_element_index = list_size - 1
+		@elements[last_element_index].next_node = list_size
+		new_element_index = list_size
+		@node.data = val
+		@node.next_node = nil
 		@elements[new_element_index] =  @node
 	end
 
-	def get_head
-	 	@elements.first
-  end
+	def get_head  
+		puts "Get list head - #{@elements.first}"
+	end
 
 	def list_elements
- 	  @elements
+		puts "List elements - #{@elements}"
+		@elements
 	end
 
 	def list_count
-		@elements.count
+		puts "List count - #{@elements.count}"
 	end
 
 	a = LinkList.new(5)
 	puts a.add(3)
 	puts a.add(23)
-	puts "Get list head - #{a.get_head}"
-	puts "List elements - #{a.list_elements}"
-	puts "List count - #{a.list_count}"
+	a.get_head
+	a.list_elements
+	a.list_count
+	
 end
