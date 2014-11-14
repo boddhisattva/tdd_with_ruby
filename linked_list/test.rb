682057090dd9e87ea38754d2601e84e350f145c8
Mohnish G J <mail4mohnish@gmail.com>
2014-11-14 14:21:02 +0530

    singly linked list prgm

--- linked_list/test.rb
+++ linked_list/test.rb
@@ -1,6 +1,5 @@
-class Test1
-  def testmeth
-  end
+class Node
+  attr_accessor :data, :next_node
 end
 
 class LinkList
@@ -13,4 +12,37 @@ class LinkList
     @node.next_node = nil
     @elements[@index] = @node
   end
+
+  def add(val)
+    @node = Node.new #every new element that has to be added should point to a new node correponding to it.
+    list_size = @elements.size
+    last_element_index = list_size - 1
+    @elements[last_element_index].next_node = list_size
+    new_element_index = list_size
+    @node.data = val
+    @node.next_node = nil
+    @elements[new_element_index] =  @node
+  end
+
+  def get_head  
+    puts "Get list head - #{@elements.first}"
+  end
+
+  def list_elements
+    puts "List elements - #{@elements}"
+    @elements
+  end
+
+  def list_count
+    puts "List count - #{@elements.count}"
+  end
+
+  a = LinkList.new(5)
+  puts a.add(3)
+  puts a.add(23)
+  a.get_head
+  a.list_elements
+  a.list_count
+  
+end
 end  
