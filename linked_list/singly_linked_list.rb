92c0372d0419dd4c10cb74bdafbb6dbf37d7da0a
Mohnish G J <mail4mohnish@gmail.com>
2014-11-16 14:29:27 +0530

    implements a singly linked list without hash - wip

--- linked_list/singly_linked_list.rb
+++ linked_list/singly_linked_list.rb
@@ -3,46 +3,76 @@ class Node
 end
 
 class LinkList
-  attr_accessor :elements, :index
+  attr_accessor :head
   def initialize(val)
-    @elements = {}
-    get_node(val)
-    @index = 0
-    @elements[@index] = @node
+    @head = get_node(val)
   end
 
   def get_node(val)
     @node = Node.new # each linked list has a node, 'has a' implying composition.
     @node.data = val
     @node.next_node = nil
+    @node
   end
 
-  def add(val)s
-    get_node(val) #every new element that has to be added should point to a new node correponding to it.
-    list_size = @elements.size
-    last_element_index = list_size - 1
-    @elements[last_element_index].next_node = list_size
-    new_element_index = list_size
-    @elements[new_element_index] =  @node
+  def add(val)
+    current_pointer = @head
+    while current_pointer.next_node != nil
+      puts "Val is - #{val}"
+      puts "Curr pointer data:- #{current_pointer.data}"
+      puts "Curr pointer, next node:- #{current_pointer.next_node}"
+      current_pointer = current_pointer.next_node
+    end
+    current_pointer.next_node = get_node(val)
+  end
+
+  def add_beginning(val)
+    new_node = get_node(val)    
+    new_node.next_node = @head
+    @head = new_node
   end
 
   def get_head  
-    puts "Get list head - #{@elements.first}"
+    puts "Get Head - #{@head.data} | #{@head.next_node}"
   end
 
   def list_elements
-    puts "List elements - #{@elements}"
+    list_elements = []
+    current_pointer = @head
+    while current_pointer != nil
+      list_elements << current_pointer.data
+      current_pointer = current_pointer.next_node
+    end
+    list_elements.each do |element| 
+      print "#{element} -> "
+     end
   end
 
   def list_count
-    puts "List count - #{@elements.count}"
+    count = 0
+    current_pointer = @head
+    while current_pointer.next_node != nil
+      puts "#{current_pointer.data}"
+      current_pointer = current_pointer.next_node
+      puts "hi"
+      count += 1
+    end
+    count += 1 # to accomodate the count of the last node whose next_node has a value nil and which thereby wasn't counted in the while loop
+    puts "List count - #{count}"
   end
 
   a = LinkList.new(5)
   a.add(3)
   a.add(23)
-  a.get_head
-  a.list_elements
-  a.list_count
-  
+  a.add(55)
+  a.add_beginning(16)
+  a.add(39)
+  #a.add(23)
+  puts a
+  puts "a inspect - #{a.inspect}" 
+  puts "a head inspect - #{a.head.inspect}"
+  puts a.get_head
+  puts a.list_count
+  puts a.list_elements
+   
 end
